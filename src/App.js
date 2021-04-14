db3a8533ccc2f7a532c1ebb00351bfb93c1e01ef
Jack Goettle <41124102+jackg235@users.noreply.github.com>
2021-04-14 16:04:00 -0400

    zoom link

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import Home from './components/Home'
 import Projects from './components/Projects'
 
 function App() {
-  return (
-    <Router>
-      <Route exact path = '/' render={() => <Home /> }></Route>
-      <Route exact path = '/projects' render={() => <Projects /> }></Route>
-    </Router>
-  );
+    return (
+        <Router>
+            <Route exact path = '/' render={() => <Home /> }></Route>
+            <Route exact path = '/projects' render={() => <Projects /> }></Route>
+            <Route path='/zoom' component={() => {
+                window.location.href = 'https://us02web.zoom.us/j/9780978295?pwd=eVh6b1BpQ0F4aS9DbUoyMmdmWkxiZz09';
+                return null;
+            }}/>
+        </Router>
+    );
 }
 
 export default App;
