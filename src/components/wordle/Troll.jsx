8c71cb8075b5863109733c554d4852a7866f52f5
Jack Goettle <41124102+jackg235@users.noreply.github.com>
2022-02-04 15:01:50 -0500

    take away troll break

--- src/components/wordle/Troll.jsx
+++ src/components/wordle/Troll.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import troll from '../../static/images/troll.png'
+
 class Troll extends React.Component {
 
     componentDidMount() {
-        setTimeout(function() { window.open("/trollpic") }, 100);
+        setTimeout(function() { window.open("/troll") }, 100);
         //window.open("/home");
     }
 
     render() {
         return (
           <div className="container">
+              <img style={{width:"100%"}} src={troll} />
           </div>
         );
       }
