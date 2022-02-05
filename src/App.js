cc05cf3e3be1ee9d9a237f7f7827294f14e620fa
Jack Goettle <41124102+jackg235@users.noreply.github.com>
2022-02-05 14:26:56 -0500

    fix paths

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
     return (
         <Router>
             <Route exact path = '/' render={() => <Home /> }></Route>
-            <Route exact path = '/projects' render={() => <Projects /> }></Route>
-            <Route exact path = '/wordle' render={() => <Wordle /> }></Route>
-            <Route exact path = '/troll' render={() => <Troll /> }></Route>
+            <Route exact path = '/projects/' render={() => <Projects /> }></Route>
+            <Route exact path = '/wordle/' render={() => <Wordle /> }></Route>
+            <Route exact path = '/troll/' render={() => <Troll /> }></Route>
             <Route exact path = '/trollpic' render={() => <TrollPic /> }></Route>
-            <Route path='/zoom' component={() => {
+            <Route path='/zoom/' component={() => {
                 window.location.href = 'https://us02web.zoom.us/j/9780978295?pwd=eVh6b1BpQ0F4aS9DbUoyMmdmWkxiZz09';
                 return null;
             }}/>
