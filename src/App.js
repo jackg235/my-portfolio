e597a9d9a69002985fa6a02bde4ec2a064bca637
Jack Goettle <41124102+jackg235@users.noreply.github.com>
2022-02-05 14:41:29 -0500

    maybe

--- src/App.js
+++ src/App.js
@@ -4,18 +4,16 @@ import Home from './components/Home'
 import Projects from './components/Projects'
 import Wordle from './components/wordle/Wordle'
 import Troll from './components/wordle/Troll'
-import TrollPic from './components/wordle/TrollPic'
 
 
 function App() {
     return (
         <Router>
             <Route exact path = '/' render={() => <Home /> }></Route>
-            <Route exact path = '/projects/' render={() => <Projects /> }></Route>
-            <Route exact path = '/wordle/' render={() => <Wordle /> }></Route>
-            <Route exact path = '/troll/' render={() => <Troll /> }></Route>
-            <Route exact path = '/trollpic' render={() => <TrollPic /> }></Route>
-            <Route path='/zoom/' component={() => {
+            <Route  path = '/projects' render={() => <Projects /> }></Route>
+            <Route  path = '/wordle' render={() => <Wordle /> }></Route>
+            <Route  path = '/troll' render={() => <Troll /> }></Route>
+            <Route path='/zoom' component={() => {
                 window.location.href = 'https://us02web.zoom.us/j/9780978295?pwd=eVh6b1BpQ0F4aS9DbUoyMmdmWkxiZz09';
                 return null;
             }}/>
